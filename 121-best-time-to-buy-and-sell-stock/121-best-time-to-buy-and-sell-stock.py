57736e8d2b37c230356befae15ecd88d70687096
Joshua <48045950+Joshuaarr@users.noreply.github.com>
2022-03-17 16:52:44 +0800

    Time: 1249 ms (67.23%), Space: 25.1 MB (17.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,20 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        思路：
+        使用 left right 指针，左指针固定，右指针每次向后移一位
+        每次循环比对，找最大的 profit
+        当 diff 小于 0 时，更新左指针到右指针处
+
+        特殊情况：
+        len 为 1 时，输出 0
+        '''
         profit = 0
-        n = len(prices)
-        if n == 1 :
-            return profit
-        i = 0
-        left = prices[i]
-        while i < n-1:
-            i += 1
-            right = prices[i]
-            diff = right - left
-            if diff > profit :
+        left = prices[0]
+        for i in range(1, len(prices)):
+            diff = prices[i] - left
+            if diff > profit:
                 profit = diff
-            elif diff < 0 :
-                left = right
+            elif diff < 0:
+                left = prices[i]
         return profit
-        
